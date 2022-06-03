08231641cac6b5cd4e0e39f766f33cd25c9cdfc9
Aisha Tammy <aisha@bsd.ac>
2022-06-03 14:53:40 -0400

    more error handling in BDBStore
    Signed-off-by: Aisha Tammy <aisha@bsd.ac>

--- lib/BDBStore.go
+++ lib/BDBStore.go
@@ -33,7 +33,8 @@ import (
    SGet(key string) ([]byte, error) - get value of 'key'          (as string)
 */
 type BDBStore struct {
-	DB *badger.DB
+	DB     *badger.DB
+	Logger *BotLogger
 }
 
 func NewBDBStore(dbPath string, b *BotLogger) (*BDBStore, error) {
@@ -45,6 +46,7 @@ func NewBDBStore(dbPath string, b *BotLogger) (*BDBStore, error) {
 	}
 	bdbStore := new(BDBStore)
 	bdbStore.DB = bdb
+	bdbStore.Logger = b
 	return bdbStore, nil
 }
 
@@ -112,35 +114,60 @@ func (s *BDBStore) SDelete(key string) error {
 
 func (s *BDBStore) SaveFilterID(userID id.UserID, filterID string) {
 	uid := "userid_filter_" + userID.String()
-	_ = s.Set([]byte(uid), []byte(filterID))
+	err := s.Set([]byte(uid), []byte(filterID))
+	if err != nil {
+		s.Logger.Errorf("Error in SaveFilterID(%s, %s): %v", userID.String(), filterID, err)
+	}
 }
 
 func (s *BDBStore) LoadFilterID(userID id.UserID) string {
 	uid := "userid_filter_" + userID.String()
-	filter, _ := s.Get([]byte(uid))
+	filter, err := s.Get([]byte(uid))
+	if err != nil {
+		s.Logger.Errorf("Error in LoadFilterID(%s): %v", userID.String(), err)
+		return ""
+	}
 	return string(filter[:])
 }
 
 func (s *BDBStore) SaveNextBatch(userID id.UserID, nextBatchToken string) {
 	uid := "userid_batch_" + userID.String()
-	s.Set([]byte(uid), []byte(nextBatchToken))
+	err := s.Set([]byte(uid), []byte(nextBatchToken))
+	if err != nil {
+		s.Logger.Errorf("Error in SaveNextBatch(%s, %s): %v", userID.String(), nextBatchToken, err)
+	}
 }
 
 func (s *BDBStore) LoadNextBatch(userID id.UserID) string {
 	uid := "userid_batch_" + userID.String()
-	batch, _ := s.Get([]byte(uid))
+	batch, err := s.Get([]byte(uid))
+	if err != nil {
+		s.Logger.Errorf("Error in LoadNextBatch(%s): %v", userID.String(), err)
+		return ""
+	}
 	return string(batch[:])
 }
 
 func (s *BDBStore) SaveRoom(room *mautrix.Room) {
 	rdata, _ := EncodeRoom(room)
 	rid := "roomid_" + room.ID.String()
-	s.Set([]byte(rid), rdata)
+	err := s.Set([]byte(rid), rdata)
+	if err != nil {
+		s.Logger.Errorf("Error in SaveRoom(%d): %v", room.ID.String(), err)
+	}
 }
 
 func (s *BDBStore) LoadRoom(roomID id.RoomID) *mautrix.Room {
 	rid := "roomid_" + roomID.String()
-	rdata, _ := s.Get([]byte(rid))
+	rdata, err := s.Get([]byte(rid))
+	if err != nil {
+		s.Logger.Errorf("Error in LoadRoom(%s) while getting data: %v", roomID.String(), err)
+		return nil
+	}
 	room, _ := DecodeRoom([]byte(rdata))
+	if err != nil {
+		s.Logger.Errorf("Error in LoadRoom(%s) while decoding room data: %v", roomID.String(), err)
+		return nil
+	}
 	return room
 }
